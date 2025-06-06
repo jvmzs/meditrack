8261acc9907954b3abc188b400552203080612d8
João Menezes <78425478+jvmzs@users.noreply.github.com>
2025-06-06 01:55:18 -0300

    Add files via upload

--- src/constants/UIvariables.java
+++ src/constants/UIvariables.java
@@ -7,6 +7,10 @@ public class UIvariables {
 
     //Cores
     public static final Color WHITE_COLOR = new Color(255, 255, 255);
+<<<<<<< HEAD
+=======
+    public static final Color GRAY_COLOR = new Color(211, 211, 211);
+>>>>>>> 5960a5e (primeiro commit)
     public static final Color BACKGROUND_PANEL_BLUE = new Color(24,120,141);
     public static final Color BLACK_COLOR = new Color(0,0,0);
     public static final Color BTN_COLOR = new Color(34,34,34);
@@ -15,17 +19,36 @@ public class UIvariables {
     public static final Color ERROR_COLOR = new Color(255,0,0);
     public static final Color ERRO_BOXLABEL = new Color(248,215,215);
     public static final Color ERROR_LABEL = new Color(184,72,72);
+<<<<<<< HEAD
 
 
     //fontes
     public static final Font FONT_TEXT = new Font("Poppins", Font.BOLD, 20);
+=======
+    public static final Color COLOR_ENFPanel = new Color(179,229,252);
+    public static final Color COLOR_CALC = new Color(179,229,252);
+    public static final Color COLOR_CALC_BUTTON = new Color(2,119,189);
+
+    public static final Font FONT_TEXT = new Font("Poppins", Font.BOLD, 20);
+    public static final Font FONT_15 = new Font("Poppins", Font.BOLD, 15);
+>>>>>>> 5960a5e (primeiro commit)
     public static final Font FONT_LOGO = new Font("Poppins", Font.BOLD, 24);
     public static final Font FONT_INPUT = new Font("Poppins", Font.PLAIN,24);
     public static final Font FONT_BUTTON = new Font("Poppins", Font.BOLD,26);
     public static final Font FONT_TITLE = new Font("Poppins", Font.BOLD, 30);
+<<<<<<< HEAD
     public static final Font FONT_INPUT_RECEPCIONISTA = new Font("Poppins", Font.BOLD,20);
     public static final Font FONT_INPUT_RECEPCIONISTA_HOVER = new Font("Poppins", Font.BOLD, 25);
     public static final Font FONT_ERROR = new Font("Arial", Font.BOLD, 20);
 
 
 }
+=======
+    public static final Font FONT_TITLE2 = new Font("Poppins", Font.BOLD, 20);
+    public static final Font FONT_INPUT_RECEPCIONISTA = new Font("Poppins", Font.BOLD,20);
+    public static final Font FONT_INPUT_RECEPCIONISTA_HOVER = new Font("Poppins", Font.BOLD, 25);
+    public static final Font FONT_ERROR = new Font("Arial", Font.BOLD,20);
+
+
+}
+>>>>>>> 5960a5e (primeiro commit)
